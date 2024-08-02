72e2a6d2f3a0caa3b701ef7c7acd58777ca7a690
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2024-08-02 19:03:42 +0100

    upgraded laravel 11

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +17,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        // 'name',
+        'name',
         'email',
         'password',
     ];
@@ -34,16 +33,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Get the attributes that should be cast.
      *
-     * @var array<string, string>
+     * @return array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function feeds()
+    protected function casts(): array
     {
-        return $this->hasMany(FeedList::class);
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UserController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/', [UserController::class, 'index'])->name('dashboard');
-    Route::get('/feed/{id}', [UserController::class, 'feed'])->name('feed');
-    Route::post('/subscribe', [UserController::class, 'subscribe'])->name('subscribe');
-    Route::post('/unsubscribe', [UserController::class, 'unsubscribe'])->name('unsubscribe');
-
-});
-
-Route::group(['middleware' => 'guest'], function() {
-    Route::get('/register', [RegisterController::class, 'index'])->name('register');
-    Route::post('/register-user', [RegisterController::class, 'store'])->name('register.user');
-    Route::get('login', [LoginController::class, 'index'])->name('login');
-    Route::post('login-user', [LoginController::class, 'loginUser'])->name('login.user');
-    Route::get('signout', [LoginController::class, 'signOut'])->name('signout');
+Route::get('/', function () {
+    return view('welcome');
 });
